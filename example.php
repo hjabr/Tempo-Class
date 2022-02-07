0f37c00b08739c8a7f98e8499b887b24ac18e66e
Hesham Jabr <92955459+hjabr@users.noreply.github.com>
2022-02-06 16:19:00 -0800

    Create example.php

---
+++ example.php
@@ -0,0 +1,30 @@
+<?php
+
+// include Tempo class
+require_once "tempo.class.php";
+
+
+/**
+ * 
+ *      Example
+ * 
+ */
+
+try {
+        
+        $name = "Hesham Jabr";
+        // $name1 = "Hesham Jabr";
+        // $name2 = "Hesham Jabr";
+        // $name3 = "Hesham Jabr";
+
+        $temp = new Tempo("./tempo","home");
+        $temp->set("name", $name);
+        // $temp->set("name1", $name1);
+        // $temp->set("name2", $name2);
+        // $temp->set("name3", $name3);
+        $temp->end();
+
+        
+} catch (Exception $e) {
+        echo $e->getMessage();
+}
