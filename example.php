ce7a0bf567f59bcaf53e86bfec27e699d3962c07
Hesham Jabr <92955459+hjabr@users.noreply.github.com>
2022-02-06 16:25:48 -0800

    Update example.php

--- example.php
+++ example.php
@@ -4,7 +4,7 @@
  * 
  *   Tempo - a simple PHP class for rendering PHP templates
  * 
- *   Developed by Hesham Jabr
+ *   Created by Hesham Jabr
  *   email: info@jabr.dev
  *   website: www.jabr.dev
  * 
